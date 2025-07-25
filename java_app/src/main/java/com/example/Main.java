7b0ae04b008da0c135c15607e00afa354d1cfa71
suhal vemu <vemusuhal@gmail.com>
2025-07-25 14:33:21 +0530

    First Mono repo lab.

---
+++ java_app/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package src.main.java.com.example;
+
+public class Main {
+      public static void main(String[] args) {
+            //assinging a value to a variable
+            String a = "Hello, World!";
+            System.out.println(a);
+            // Additional code can be added heres
+
+      }
+
+}
